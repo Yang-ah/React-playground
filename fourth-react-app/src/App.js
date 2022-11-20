b4787e5a4a1e70d7daad23c497fa7203ad275486
Yang-ah <yangah0409@gmail.com>
2022-11-20 19:47:13 +0900

    practiced using useEffect(callbackFn, [deps])

--- fourth-react-app/src/App.js
+++ fourth-react-app/src/App.js
@@ -2,17 +2,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  function onClick() {
-    setCounter((prev) => prev + 1);
-  }
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setCounter((prev) => prev + 1);
+
+  const onChange = (event) => setKeyword(event.target.value);
+
   console.log("I run all the time");
 
   const runOnlyOnce = () => console.log("I run only one time.");
 
   useEffect(runOnlyOnce, []);
 
+  useEffect(() => {
+    if (keyword !== keyword.length > 5) {
+      console.log("Search for", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search hear..."
+      />
       <h1>Clicked {counter} times</h1>
       <button onClick={onClick}>Click me!</button>
     </div>
