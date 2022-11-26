624a0bdd6b3c1f0c25200866ca279c317b7c0e0b
Yang-ah <yangah0409@gmail.com>
2022-11-26 19:10:23 +0900

    Cryptocurrencies - practiced useEffect(), useState

---
+++ sixth-react-app/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [inputValue, exchange] = useState("");
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((resonse) => resonse.json())
+      .then((json) => {
+        setLoading(false);
+        setCoins(json);
+        console.log(json[0]);
+      });
+  }, []);
+
+  const inputCng = (e) => {
+    exchange(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>The Coins!</h1>
+      {loading ? <h3>Loading....</h3> : null}
+      <label htmlFor="haveDollar">I have $</label>
+      <input id="haveDollar" value={inputValue} onChange={inputCng} />
+
+      <ul>
+        {coins.map((coin) => (
+          <li key={coin.id}>
+            {coin.name} ({coin.symbol}) :{" "}
+            {inputValue == false
+              ? coin.quotes.USD.price.toFixed(2)
+              : ((1 / coin.quotes.USD.price) * inputValue).toFixed(2)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
