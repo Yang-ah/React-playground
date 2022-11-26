cdafbdd533918dfa51bd19a63a73c485f67e0df2
Yang-ah <yangah0409@gmail.com>
2022-11-26 22:29:08 +0900

    use async,await instead of then

--- seventh-react-app/src/App.js
+++ seventh-react-app/src/App.js
@@ -3,16 +3,16 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    fetch(
+  const getMovies = async () => {
+    const response = await fetch(
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setLoading(false);
-        setMovies(json.data.movies);
-        console.log(movies);
-      });
+    );
+    const json = await response.json();
+    setLoading(false);
+    setMovies(json.data.movies);
+  };
+  useEffect(() => {
+    getMovies();
   }, []);
   return (
     <div>
